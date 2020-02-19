9169d2a0d2d20c00382754a06b085fe54e9eb53d
Molly Clabots <molly.clabots@gmail.com>
2020-02-19 14:58:18 -0600

    Adding temporary environment variables to the project

--- cat/weather.py
+++ cat/weather.py
@@ -1,8 +1,12 @@
-import requests
 from weather_api_key import key
+
+import os
+
+import requests
 from pprint import pprint 
 
 def main():
+    keyy = os.environ.get('WEATHER_KEY')
     url = 'https://api.openweathermap.org/data/2.5/weather?q=minneapolis,us&units=imperial&appid=' + key()
     data = requests.get(url).json()
     weather_description = data['weather'][0]['description']
@@ -10,7 +14,7 @@ def main():
     temp_feels = data['main']['feels_like']
 
     # pprint(data)
-    print(f'Today in Minneapolis there are {weather_description} out.\n\nThe temperature is {temp_f} but it feels like {temp_feels}.\n')
+    print(f'Today in Minneapolis there are {weather_description} out.\n\nThe temperature is {temp_f:.2f}F but it feels like {temp_feels:.2f}F.\n')
 
 if __name__ == '__main__':
     main()
